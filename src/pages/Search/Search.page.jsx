115d8f4e62547833de8f73019cd480e83d163e67
Carlo Bruno <carlobruno_fb@hotmail.com>
2020-09-28 21:48:52 -0500

    linted code

--- src/pages/Search/Search.page.jsx
+++ src/pages/Search/Search.page.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 import SearchFeed from '../../components/SearchFeed';
 
-
 import './Search.styles.css';
 
-
 function SearchPage() {
-    const { query } = useParams();
+  const { query } = useParams();
 
-    return (
-        <section className="Search">
-            <SearchFeed query={query}/>
-        </section>
-    );
+  return (
+    <section className="Search">
+      <SearchFeed query={query} />
+    </section>
+  );
 }
 
 export default SearchPage;
--- src/pages/Watch/Watch.page.jsx
+++ src/pages/Watch/Watch.page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 import './Watch.styles.css';
 
@@ -9,24 +9,23 @@ import VideoPlayer from '../../components/VideoPlayer';
 import RecommendationFeed from '../../components/RecommendationFeed';
 
 function WatchPage() {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    
-    return (
-        <section className="Watch">
-            <Grid container md={12}>
-                <Grid item md={9} className="WatchPageVideoPlayer" >
-                    <VideoPlayer videoId={id}/>
-                </Grid>
-                
-                <Grid container item md={3} justify="center">
-                    <Grid container item md={12}  >
-                        <RecommendationFeed videoId={id}/>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </section>
-    );
+  return (
+    <section className="Watch">
+      <Grid container md={12}>
+        <Grid item md={9} className="WatchPageVideoPlayer">
+          <VideoPlayer videoId={id} />
+        </Grid>
+
+        <Grid container item md={3} justify="center">
+          <Grid container item md={12}>
+            <RecommendationFeed videoId={id} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </section>
+  );
 }
 
 export default WatchPage;
